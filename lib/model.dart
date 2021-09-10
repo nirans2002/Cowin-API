457b7afefb83cd04f86fe9e0089ea5d6e2d68979
Niran S Narayanan <nirans2002@gmail.com>
2021-09-10 22:17:49 +0530

    test init

---
+++ lib/model.dart
@@ -0,0 +1,33 @@
+class Model {
+  var states;
+  List<Item> items;
+
+  Model({required this.states, required this.items});
+
+  factory Model.fromJson(Map<String, dynamic> json) {
+    return Model(
+        items: List<Item>.from(json['states'].map((x) => Item.fromJson(x))),
+        states: null);
+  }
+}
+
+class Item {
+  // ignore: non_constant_identifier_names
+  int state_id;
+  // ignore: non_constant_identifier_names
+  String state_name;
+
+  Item({
+    // ignore: non_constant_identifier_names
+    required this.state_id,
+    // ignore: non_constant_identifier_names
+    required this.state_name,
+  });
+
+  factory Item.fromJson(Map<String, dynamic> json) {
+    return Item(
+      state_id: json['state_id'],
+      state_name: json['state_name'],
+    );
+  }
+}
---
+++ lib/state_search.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'model.dart';
+import 'dart:convert';
+
+class StateSearch extends StatefulWidget {
+  const StateSearch({Key? key}) : super(key: key);
+
+  @override
+  _StateSearchState createState() => _StateSearchState();
+}
+
+// final List stateList;
+Future<Model> getStates() async {
+  var model;
+  var response = await http.get(
+    Uri.parse('https://cdn-api.co-vin.in/api/v2/admin/location/states'),
+    headers: {"Accept": "application/json"},
+  );
+  if (response.statusCode == 200) {
+    var jsonString = response.body;
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+    model = Model.fromJson(jsonMap);
+    print(Item);
+
+    return model;
+  } else {
+    throw Exception('Failed to fetch !');
+  }
+}
+
+class _StateSearchState extends State<StateSearch> {
+  late Future<Model> model;
+  @override
+  void initState() {
+    super.initState();
+    model = getStates();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("API"),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text("Select State"),
+            Center(
+              child: ElevatedButton(
+                onPressed: getStates,
+                child: Text("press"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
